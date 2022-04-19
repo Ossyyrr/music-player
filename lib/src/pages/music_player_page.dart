9f9321292e951e29d233a963886c563300f71d7f
Ossyyrr <ossyyrr@hotmail.com>
2022-04-19 12:11:23 +0200

    layout

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:music_player/src/helpers/helpers.dart';
 import 'package:music_player/src/widgets/custom_appbar.dart';
 
 class MusicPlayerPage extends StatelessWidget {
@@ -8,13 +9,107 @@ class MusicPlayerPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: SafeArea(
-      child: Column(
-        children: const [
-          CustomAppbar(),
-          ImageDiscoDuration(),
+      child: Stack(
+        children: [
+          const Background(),
+          Column(
+            children: const [
+              CustomAppbar(),
+              ImageDiscoDuration(),
+              TituloPlay(),
+              Expanded(child: Lyrics()),
+            ],
+          ),
         ],
       ),
     ));
+  }
+}
+
+class Background extends StatelessWidget {
+  const Background({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.height * 0.7,
+      decoration: const BoxDecoration(
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(60)),
+        gradient: LinearGradient(
+          begin: Alignment.centerLeft,
+          end: Alignment.center,
+          colors: [
+            Color(0xff33333e),
+            Color(0xff201e28),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Lyrics extends StatelessWidget {
+  const Lyrics({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final lyrics = getLyrics();
+
+    return ListWheelScrollView(
+        physics: const BouncingScrollPhysics(),
+        itemExtent: 42,
+        diameterRatio: 2,
+        children: lyrics
+            .map((linea) => Text(
+                  linea,
+                  style: TextStyle(color: Colors.white.withOpacity(0.6), fontSize: 20),
+                ))
+            .toList());
+  }
+}
+
+class TituloPlay extends StatelessWidget {
+  const TituloPlay({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 40),
+      margin: const EdgeInsets.only(top: 40),
+      child: Row(
+        children: [
+          Column(
+            children: [
+              Text(
+                'Far Away',
+                style: TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 30),
+              ),
+              Text(
+                '-Breaking Benjamin-',
+                style: TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 15),
+              ),
+            ],
+          ),
+          const Spacer(),
+          FloatingActionButton(
+            elevation: 0,
+            highlightElevation: 0,
+            onPressed: () {
+              // TODO boton
+            },
+            backgroundColor: const Color(0xfff8cb51),
+            child: const Icon(Icons.play_arrow),
+          )
+        ],
+      ),
+    );
   }
 }
 
@@ -26,9 +121,49 @@ class ImageDiscoDuration extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 30),
+      margin: const EdgeInsets.only(top: 70),
       child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: const [
           ImagenDisco(),
+          BarraProgres(),
+        ],
+      ),
+    );
+  }
+}
+
+class BarraProgres extends StatelessWidget {
+  const BarraProgres({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final estilo = TextStyle(color: Colors.white.withOpacity(0.4));
+    return Container(
+      child: Column(
+        children: [
+          Text('00:00', style: estilo),
+          const SizedBox(width: 20),
+          Stack(
+            alignment: Alignment.bottomCenter,
+            children: [
+              Container(
+                width: 3,
+                height: 230,
+                color: Colors.white.withOpacity(0.2),
+              ),
+              Container(
+                width: 3,
+                height: 100,
+                color: Colors.white.withOpacity(0.8),
+              ),
+            ],
+          ),
+          const SizedBox(width: 20),
+          Text('00:00', style: estilo),
         ],
       ),
     );
