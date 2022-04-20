4fb0660e615be0c174616414d5712aef181ff84e
ossyyrr <ossyyrr@hotmail.com>
2022-04-20 18:01:59 +0200

    wip

--- lib/src/pages/music_player_page.dart
+++ lib/src/pages/music_player_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:music_player/src/widgets/background.dart';
-import 'package:music_player/src/widgets/custom_appbar.dart';
 import 'package:music_player/src/widgets/image_disco_duration.dart';
 import 'package:music_player/src/widgets/lyrics.dart';
 import 'package:music_player/src/widgets/titulo_play.dart';
@@ -17,7 +16,9 @@ class MusicPlayerPage extends StatelessWidget {
           const Background(),
           Column(
             children: const [
-              CustomAppbar(),
+              //   CustomAppbar(),
+              //   SizedBox(height: 25),
+              ScrollDisco(),
               ImageDiscoDuration(),
               TituloPlay(),
               SizedBox(height: 25),
@@ -27,5 +28,57 @@ class MusicPlayerPage extends StatelessWidget {
         ],
       ),
     ));
+  }
+}
+
+class ScrollDisco extends StatelessWidget {
+  const ScrollDisco({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 110,
+      width: double.infinity,
+      child: ListView(
+        scrollDirection: Axis.horizontal,
+        children: const [
+          Song(),
+          Song(),
+          Song(),
+          Song(),
+          Song(),
+          Song(),
+          Song(),
+          Song(),
+        ],
+      ),
+    );
+  }
+}
+
+class Song extends StatelessWidget {
+  const Song({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 8),
+        color: Colors.red,
+        width: 100,
+        height: 100,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Image(
+              image: AssetImage('assets/motomami.jpeg'),
+              fit: BoxFit.contain,
+            ),
+            Text('Cancion x')
+          ],
+        ));
   }
 }
